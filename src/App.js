4c1f34a9a1af787ab447d966b0f7a20d59035fe5
Ulfsaar-dev <wangtoby1125@outlook.com>
2020-02-03 16:55:16 -0800

    Redux cart completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+// In a component [1].
+ 
+/*
+// then [2]
+
+*/
 function App() {
+  const [count, setCount] = useState(0); 
+  const usePrevious = (value) => {
+    const ref = useRef();
+  
+    useEffect(() => {
+      ref.current = value;
+    }, [value]);   
+    return ref.current;
+  }
+  let prevCount = usePrevious(count);
+  console.log(prevCount);
+
+  const buttonclick = () => {
+    setCount(count +1)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +33,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={buttonclick}>ttttt</button>
         <a
           className="App-link"
           href="https://reactjs.org"
